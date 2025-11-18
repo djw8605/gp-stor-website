3578b4783927d94448e8b4898295ecd08b5fe5ba
Derek Weitzel <djw8605@gmail.com>
2025-11-18 16:20:47 -0600

    Removing base of gp-stor.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,6 @@ export default defineConfig({
     // Otherwise default to root when developing locally.
   integrations: [
       starlight({
-		  base: "https://gp-stor.org",
           title: 'GP-STOR',
           description: 'Great Plains Scalable Tiered Object Repository',
           logo: { src: './src/assets/logo.png', alt: 'GP-STOR', replacesTitle: true },
